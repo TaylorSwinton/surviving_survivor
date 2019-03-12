eee03f19415aca285aa2919f7f989bf5d93c7e83
TaylorSwinton <taylorswinton1@gmail.com>
2019-03-12 16:58:57 -0500

    added scraping class

--- lib/survivingsurvivorfile/scraper.rb
+++ lib/survivingsurvivorfile/scraper.rb
@@ -2,6 +2,30 @@
 #require 'nokogiri'
 #require 'pry'
 
-class SurvivingSurvivor::Scraper
-    
+class SurvivingSurvivor::ScraperKoah
+    def self.scrape_season_information_k
+        
+    end
+
+    def self.scrape_castaways_k
+        
+    end
+
+    def self.scrape_episode_k
+        
+    end
+end
+
+class SurvivingSurvivor::ScraperWorlds
+    def self.scrape_season_information_w
+        
+    end
+
+    def self.scrape_castaways_w
+        
+    end
+
+    def self.scrape_episode_w
+        
+    end
 end
